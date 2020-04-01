d901132633459f3f9ea08c04213769c27a8dfd9c
Carlos L Molina <carlos.molina@codeimmersives.com>
2020-04-01 15:30:28 -0400

    changes

--- add-button.js
+++ add-button.js
@@ -1,3 +1,26 @@
+/*  attempt #1 const taskField = document.querySelector(".todo-input")
+​
+document.querySelector('.add-todo').addEventListener("click", function() {
+            if (taskField.value.length > 1 && taskField.value.length < 100) {
+                return printTodos(todos)
+                
+            }
+        }) */
+        
+        
+        ​
+        ​
+                /* attempt #2 const taskField = document.querySelector(".todo-input")
+                const text = taskField.value;
+            
+                document.querySelector('.add-todo').addEventListener("click", function() {
+                    if (taskField.value.length > 1 && taskField.value.length < 100) {
+                    return printTodo(addTodo(input.value));
+                        
+                        
+                    }
+                })*/
+
 const text = document.querySelector('.todo-input').value
         const priority = document.querySelector('.priority').value
         
